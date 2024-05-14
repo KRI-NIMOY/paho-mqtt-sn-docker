7a5898172f73b4ecf9b94e843c4775d9e7a05572
Ken Witham <kennywitham4@gmail.com>
2024-05-14 18:58:32 -0400

    Remove CLI sets since Docker doesn't allow

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -1,51 +1,62 @@
 #!/bin/sh
 
-function parse_args()
-{
-    while [ $# -gt 0 ]
-    do
-        echo $1
-        case $1 in
-        --broker-name)
-            BROKER_NAME=$2
-            shift
-            shift
-            ;;
-        --broker-port)
-            BROKER_PORT=$2
-            shift
-            shift
-            ;;
-        --gw-id)
-            GW_ID=$2
-            shift
-            shift
-            ;;
-        --gw-name)
-            GW_NAME=$2
-            shift
-            shift
-            ;;
-        --xbee-dev)
-            XBEE_DEV=$2
-            shift
-            shift
-            ;;
-        *)
-            shift
-            ;;
-        esac
-    done
-}
+# getid() {
+#    ps aux | grep -v grep
+# }
 
-parse_args "$@"
+# GW_ID="$(getid)"
 
-PAHO_CONF="/etc/paho/gateway.conf"
+# function parse_args()
+# {
+#     while [ $# -gt 0 ]
+#     do
+#         echo $1
+#         case $1 in
+#         --broker-name)
+#             echo $2
+#             BROKER_NAME=$2
+#             shift
+#             shift
+#             ;;
+#         --broker-port)
+#             echo $2
+#             BROKER_PORT=$2
+#             shift
+#             shift
+#             ;;
+#         --gw-id)
+#             echo $2
+#             GW_ID=$2
+#             shift
+#             shift
+#             ;;
+#         --gw-name)
+#             echo $2
+#             GW_NAME=$2
+#             shift
+#             shift
+#             ;;
+#         --xbee-dev)
+#             echo $2
+#             XBEE_DEV=$2
+#             shift
+#             shift
+#             ;;
+#         *)
+#             shift
+#             ;;
+#         esac
+#     done
+# }
 
-if ! [ -z ${BROKER_NAME+x} ]; then sed -i 's/^BrokerName.*$/BrokerName='$BROKER_NAME'/' $PAHO_CONF; fi
-if ! [ -z ${BROKER_PORT+x} ]; then sed -i 's/^BrokerPortNo.*$/BrokerPortNo='$BROKER_PORT'/' $PAHO_CONF; fi
-if ! [ -z ${GW_ID+x} ]; then sed -i 's/^GatewayID.*$/GatewayID='$GW_ID'/' $PAHO_CONF; fi
-if ! [ -z ${GW_NAME+x} ]; then sed -i 's/^GatewayName.*$/GatewayName='$GW_NAME'/' $PAHO_CONF; fi
-if ! [ -z ${XBEE_DEV+x} ]; then sed -i 's/^SerialDevice.*$/SerialDevice='$XBEE_DEV'/' $PAHO_CONF; fi
+# parse_args "$@"
+
+# PAHO_CONF="/etc/paho/gateway.conf"
+
+# if ! [ -z ${BROKER_NAME+x} ]; then sed -i "s/^BrokerName.*$/BrokerName=$BROKER_NAME/" $PAHO_CONF; fi
+# if ! [ -z ${BROKER_PORT+x} ]; then sed -i "s/^BrokerPortNo.*$/BrokerPortNo=$BROKER_PORT/" $PAHO_CONF; fi
+# if ! [ -z ${GW_ID+x} ]; then sed -i "s/^GatewayID.*$/GatewayID=$GW_ID/" $PAHO_CONF; fi
+# if ! [ -z ${GW_NAME+x} ]; then sed -i "s/^GatewayName.*$/GatewayName=$GW_NAME/" $PAHO_CONF; fi
+# if ! [ -z ${XBEE_DEV+x} ]; then sed -i "s/^SerialDevice.*$/SerialDevice=$XBEE_DEV/" $PAHO_CONF; fi
 
 /usr/local/sbin/MQTT-SNGateway -f /etc/paho/gateway.conf
